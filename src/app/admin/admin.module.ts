b6ff25f3f1fae9e2c8f1d172140dd1419f5df261
1900016083 Syauqi Rayhan Abqori <syauqi1900016083@webmail.uad.ac.id>
2021-06-19 20:43:35 +0700

    progres individu

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialDesign } from '../material/material';
+
+const routes: Routes = [
+  {
+    path:'',
+    component:AdminComponent,
+    children:[
+      {
+        path:'dashboard',
+        component:DashboardComponent
+      }
+    ]
+  }
+]
+
+@NgModule({
+  declarations: [
+    AdminComponent,
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    MaterialDesign
+  ]
+})
+export class AdminModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialDesign } from './material/material';
+import { ForgotComponent } from './auth/forgot/forgot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialDesign
   ],
   providers: [],
   bootstrap: [AppComponent]
